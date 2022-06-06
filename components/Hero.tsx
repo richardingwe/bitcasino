9ce25865e58e59d96ef3daafdd6037f6d1badaed
Richard Ingwe <richardingwe5@gmail.com>
2022-06-06 19:55:17 +0100

    perfectly set the background image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,7 +25,7 @@ const CssTextField = styled(TextField)({
 
 const Hero = () => {
 	return (
-		<main className='my-12'>
+		<main className='mt-12 mb-9'>
 			<section>
 				<div className='w-full flex items-center justify-between'>
 					<div className='flex-1'>
@@ -40,8 +40,8 @@ const Hero = () => {
 							form to the right.
 						</p>
 					</div>
-					<div className='flex-1 flex justify-end'>
-						<div className='w-[400px] bg-white p-10 flex flex-col rounded'>
+					<div className='flex-1 flex justify-end z-10'>
+						<div className='w-[400px] bg-white px-10 py-8 flex flex-col rounded'>
 							<CssTextField
 								label='CRYPTOCURRENCY CODE'
 								variant='outlined'
@@ -60,7 +60,7 @@ const Hero = () => {
 					</div>
 				</div>
 
-				<div className='mt-[32px]'>
+				<div className='mt-[32px] z-[20]'>
 					<div className='flex items-center justify-between w-[280px] py-3 border-b-[1px] border-[#9484a4]'>
 						<div className='flex items-center gap-x-8'>
 							<img src='/assets/icon.svg' alt='trophy' />
@@ -98,6 +98,16 @@ const Hero = () => {
 						</div>
 					</div>
 				</div>
+				{/* <img
+					src='/assets/figure.png'
+					className='absolute bottom-0 lg:w-[470px] -z-[0] left-0 right-0 mx-auto'
+					alt='figure'
+				/>
+				<img
+					src='/assets/bg.png'
+					className='absolute top-0 lg:w-[800px] -z-[0] right-[-300px]'
+					alt='figure'
+				/> */}
 			</section>
 		</main>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,17 +11,23 @@ const Home: NextPage = () => {
 			<Head>
 				<title>Bitcasino</title>
 				<link rel='icon' href='/favicon.ico' />
-				<link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+				<link
+					rel='preconnect'
+					href='https://fonts.gstatic.com'
+					crossOrigin='true'
+				/>
 				<link rel='preconnect' href='https://fonts.googleapis.com' />
 				<link
 					href='https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;600;700&display=swap'
 					rel='stylesheet'
 				/>
 			</Head>
-			<div className='container mx-auto py-6 lg:px-32 bg-primary'>
-				<Header />
-				<Hero />
-			</div>
+			<main className='bg-primary relative main'>
+				<div className='container mx-auto py-6 lg:px-16'>
+					<Header />
+					<Hero />
+				</div>
+			</main>
 			<div className='container mx-auto py-6 lg:px-32 bg-white'>
 				<Footer />
 			</div>
